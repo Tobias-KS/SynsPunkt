7bc212ac61e6d80b7bb85a45bc945f7bcf88a492
Anders Hoffmann <au568584@gmail.com>
2020-05-08 10:51:17 +0200

    Idk

--- Semesterprojekt/UI/Program.cs
+++ Semesterprojekt/UI/Program.cs
@@ -8,9 +8,6 @@ namespace UI
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
