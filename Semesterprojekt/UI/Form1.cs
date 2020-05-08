059a177803d8f00426d8d2292edfa631026e5557
Martin Djørby Knudsen <mart480n@edu.ucl.dk>
2020-05-08 12:33:59 +0200

    FormsDesign

--- Semesterprojekt/UI/Form1.cs
+++ Semesterprojekt/UI/Form1.cs
@@ -12,9 +12,49 @@ namespace UI
 {
     public partial class FrontPage : Form
     {
+
+
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+
         public FrontPage()
         {
             InitializeComponent();
+        }
+
+        private void CustomerButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TopPanel_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void TopPanel_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        private void CloseApp_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void MinimizeApp_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
         }
     }
 }
