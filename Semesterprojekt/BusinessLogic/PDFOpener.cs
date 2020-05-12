a056390bc792fe522870cddad3c359e17d1e4272
Anders Hoffmann <au568584@gmail.com>
2020-05-12 13:00:16 +0200

    Måske fix til PDF-filer

--- Semesterprojekt/BusinessLogic/PDFOpener.cs
+++ Semesterprojekt/BusinessLogic/PDFOpener.cs
@@ -1,20 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
 using System.Text;
 
 namespace BusinessLogic
 {
-   public class PDFOpener
+    public class PDFOpener
     {
         public static void OpenPdfFil(string pdf)
         {
             System.Diagnostics.Process.Start(pdf);
         }
+
         public static void SupportPdf()
         {
-            OpenPdfFil("SynsPunktSupport.pdf.pdf");
+            string pdf_path=
+                AppDomain.CurrentDomain.BaseDirectory + "//BusinessLogicResources//PdfFiles//SynsPunktSupport.pdf";
+            
+            OpenPdfFil(pdf_path);
         }
-        
 
+       
     }
 }
