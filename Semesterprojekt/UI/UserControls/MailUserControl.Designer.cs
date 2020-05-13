f615b03f7912a78f450e4495894ed957f2b40d52
Martin Djørby Knudsen <mart480n@edu.ucl.dk>
2020-05-13 09:26:49 +0200

    Resice mailpage

--- Semesterprojekt/UI/UserControls/MailUserControl.Designer.cs
+++ Semesterprojekt/UI/UserControls/MailUserControl.Designer.cs
@@ -39,17 +39,17 @@
             // 
             // richTextBox2
             // 
-            this.richTextBox2.Location = new System.Drawing.Point(608, 143);
+            this.richTextBox2.Location = new System.Drawing.Point(827, 152);
             this.richTextBox2.Name = "richTextBox2";
-            this.richTextBox2.Size = new System.Drawing.Size(512, 488);
+            this.richTextBox2.Size = new System.Drawing.Size(809, 703);
             this.richTextBox2.TabIndex = 1;
-            this.richTextBox2.Text = "Dear Customer of SunsPunkt\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nBest regards\nSynsPunkt\n";
+            this.richTextBox2.Text = "Dear Customer of SunsPunkt\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nBest regards\nSynsPunkt\n";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(604, 119);
+            this.label1.Location = new System.Drawing.Point(823, 125);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(91, 19);
             this.label1.TabIndex = 2;
@@ -59,7 +59,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(53, 119);
+            this.label2.Location = new System.Drawing.Point(22, 125);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(334, 19);
             this.label2.TabIndex = 3;
@@ -77,9 +77,9 @@
             "Mail5@Mail.com ",
             "Mail6@Mail.com ",
             ""});
-            this.ListOfCustomersThatHaveAcceptedNewsLetters.Location = new System.Drawing.Point(57, 147);
+            this.ListOfCustomersThatHaveAcceptedNewsLetters.Location = new System.Drawing.Point(26, 152);
             this.ListOfCustomersThatHaveAcceptedNewsLetters.Name = "ListOfCustomersThatHaveAcceptedNewsLetters";
-            this.ListOfCustomersThatHaveAcceptedNewsLetters.Size = new System.Drawing.Size(461, 487);
+            this.ListOfCustomersThatHaveAcceptedNewsLetters.Size = new System.Drawing.Size(715, 697);
             this.ListOfCustomersThatHaveAcceptedNewsLetters.TabIndex = 4;
             // 
             // comboBox1
@@ -89,7 +89,7 @@
             "All Customers",
             "Newest Customers",
             "Frequent Customers"});
-            this.comboBox1.Location = new System.Drawing.Point(608, 75);
+            this.comboBox1.Location = new System.Drawing.Point(827, 64);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(170, 29);
             this.comboBox1.TabIndex = 5;
@@ -98,7 +98,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(604, 53);
+            this.label3.Location = new System.Drawing.Point(823, 42);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(307, 19);
             this.label3.TabIndex = 6;
@@ -108,7 +108,7 @@
             // 
             this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(118)))), ((int)(((byte)(211)))));
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Location = new System.Drawing.Point(1016, 637);
+            this.button1.Location = new System.Drawing.Point(1532, 861);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(104, 34);
             this.button1.TabIndex = 7;
@@ -130,7 +130,7 @@
             this.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(5);
             this.Name = "MailUserControl";
-            this.Size = new System.Drawing.Size(1211, 774);
+            this.Size = new System.Drawing.Size(1700, 966);
             this.ResumeLayout(false);
             this.PerformLayout();
 
